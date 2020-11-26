ad6078ae1a7928d88f5190733684a58dfbe59209
Hajra Mohammad Shahid <hajra.mshahid24@gmail.com>
2020-11-26 18:22:41 +0530

    theme switcher

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
+  const [Theme,setTheme] = useState('rgb(40, 41, 41)');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor: Theme}} className="container">
+      <div className="themeColor">
+        <div onClick={() =>  setTheme(' rgb(0, 139, 139)')  }></div>
+        <div onClick={() =>  setTheme(' rgb(4, 63, 90)')  }></div>
+        <div onClick={() =>  setTheme(' rgb(117, 5, 161)')  }></div>
+        <div onClick={() =>  setTheme(' rgb(185, 7, 31)')  }></div>
+        <div onClick={() =>  setTheme(' rgb(212, 141, 7)')  }></div>
+        <div onClick={() =>  setTheme(' rgb(109, 109, 109)')  }></div>
+        <div onClick={() =>  setTheme(' rgb(40, 41, 41)')  }></div>
+      </div>
+      <div className="content">
+        <h1>The easiest way to learn React</h1>
+        <p>Learn in an interactive environment. Understand how React works not just how to build with React.</p>
+        <a>Start Learning</a>
+      </div>
+      <img src={logo} />
     </div>
   );
 }
 
-export default App;
+export default App
